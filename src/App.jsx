37551949372b5985f3e69caf527a897afa88254f
Jhonatan Gonçalves <jhonatangoncalves2002@gmail.com>
2024-07-30 15:18:24 -0300

    :sparkles: Construção da página inicial e instalção de dependências axios para consumo da API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const inputRef = useRef()
+
+  async function searchCity() {
+    const city = inputRef.current.value
+    const key = "a5711a91fefb37ec73c795feb574bb8e"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+
+    try {
+      const response = await axios.get(url)
+      console.log(response.data)
+    } catch (error) {
+      if (error.response) {
+        console.error(`Error: ${error.response.status} - ${error.response.data.message}`)
+      } else {
+        console.error('Error:', error.message)
+      }
+    }
+  }
 
   return (
     <div>
-        <h1>JhonDev Previsão do tempo</h1>
+      <h1>JhonDev Previsão do tempo</h1>
+      <input ref={inputRef} type="text" placeholder='Digite o nome da cidade' />
+      <button onClick={searchCity}>Buscar</button>
     </div>
   )
 }
