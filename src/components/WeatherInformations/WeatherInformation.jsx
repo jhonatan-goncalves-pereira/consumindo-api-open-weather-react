b16831dff8ef47ffc5ca8ac76f6d2abb3206530f
Jhonatan Gonçalves <jhonatangoncalves2002@gmail.com>
2024-08-11 09:24:41 -0300

    :sparkles: feat: Conclusão do retorno do componente WeatherInformations

--- src/components/WeatherInformations/WeatherInformation.jsx
+++ src/components/WeatherInformations/WeatherInformation.jsx
@@ -2,12 +2,20 @@ function WeatherInformation({ weather }) {
     console.log(weather);
   
     return (
+      <>
       <div>
         <h2>{weather.name}</h2>
         <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt="Weather icon" />
-        <p>Temperatura: {weather.main.temp} °C</p>
-        <p>Descrição: {weather.weather[0].description}</p>
+        <p> {Math.round(weather.main.temp)} °C</p>
+        <p> {weather.weather[0].description}</p>
       </div>
+      <div>
+        <p>Sensação Térmica: {Math.round(weather.main.feels_like)}</p>
+        <p>Umidade: {weather.main.humidity} </p>
+        <p>Pressão: {weather.main.pressure} </p>
+      </div>
+      
+    </>
     );
   }
   
