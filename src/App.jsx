ea631c080cf5485aab6be39882e48e58121d95c6
Jhonatan Gonçalves <jhonatangoncalves2002@gmail.com>
2024-08-11 09:08:25 -0300

    :boom: fix: Corrigindo de objseto vazio para null o que facilita a verificação se os dados foram carregados e adicionando uma mensagem padrão para o caso de weather ser null ou ainda não ter sido definido.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import { useState, useRef } from 'react'
-import axios from 'axios'
-import './App.css'
-import WeatherInformation from './components/WeatherInformations/WeatherInformation'
+import { useState, useRef } from 'react';
+import axios from 'axios';
+import './App.css';
+import WeatherInformation from './components/WeatherInformations/WeatherInformation';
 
 function App() {
-  const [weather, setWeather] = useState({})
-  const inputRef = useRef()
+  const [weather, setWeather] = useState(null); // Inicializa com null
+  const inputRef = useRef();
 
   async function searchCity() {
-    const city = inputRef.current.value
-    const key = import.meta.env.VITE_OPENWEATHER_API_KEY
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+    const city = inputRef.current.value;
+    const key = import.meta.env.VITE_OPENWEATHER_API_KEY;
+
+    if (!key) {
+      console.error('Chave da API não está configurada.');
+      return;
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`;
 
     try {
-      const apiWeather = await axios.get(url)
-      setWeather(apiWeather.data)
+      const apiWeather = await axios.get(url);
+      setWeather(apiWeather.data);
     } catch (error) {
       if (error.response) {
-        console.error(`Error: ${error.response.status} - ${error.response.data.message}`)
+        console.error(`Error: ${error.response.status} - ${error.response.data.message}`);
       } else {
-        console.error('Error:', error.message)
+        console.error('Error:', error.message);
       }
     }
   }
@@ -30,9 +36,13 @@ function App() {
       <input ref={inputRef} type="text" placeholder='Digite o nome da cidade' />
       <button onClick={searchCity}>Buscar</button>
 
-      <WeatherInformation weather={weather} ></WeatherInformation>
+      {weather ? (
+        <WeatherInformation weather={weather} />
+      ) : (
+        <p>Digite o nome de uma cidade para buscar a previsão do tempo.</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
