94c976ff158184446ab52a20a663e1f61e92fbe4
Jhonatan Gonçalves <jhonatangoncalves2002@gmail.com>
2024-08-11 10:02:20 -0300

    :lipstick: feat: Estilização css do componente WeatherInformation que contém as informações sobre o tempo atual

--- src/components/WeatherInformations/WeatherInformation.jsx
+++ src/components/WeatherInformations/WeatherInformation.jsx
@@ -1,23 +1,29 @@
+import './WeatherInformation.css';
+
 function WeatherInformation({ weather }) {
-    console.log(weather);
-  
-    return (
-      <>
-      <div>
+  console.log(weather);
+
+  return (
+    <>
+      <div className='weather-container'>
         <h2>{weather.name}</h2>
-        <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt="Weather icon" />
-        <p> {Math.round(weather.main.temp)} °C</p>
-        <p> {weather.weather[0].description}</p>
+        <div className="weather-info">
+          <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt="Weather icon" />
+          <p className='weather-info-temperature'> {Math.round(weather.main.temp)} °C</p>
+        </div>
+        <p className='weather-description'> {weather.weather[0].description}</p>
+
+
+        <div className='weather-details'>
+          <p>Sensação Térmica: {Math.round(weather.main.feels_like)}</p>
+          <p>Umidade: {weather.main.humidity} </p>
+          <p>Pressão: {weather.main.pressure} </p>
+        </div>
+
       </div>
-      <div>
-        <p>Sensação Térmica: {Math.round(weather.main.feels_like)}</p>
-        <p>Umidade: {weather.main.humidity} </p>
-        <p>Pressão: {weather.main.pressure} </p>
-      </div>
-      
+
     </>
-    );
-  }
-  
-  export default WeatherInformation;
-  
+  );
+}
+
+export default WeatherInformation;
